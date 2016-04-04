58d44da682cfc40990dd9714bb5d4c59739db8f2
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2016-04-04 20:26:02 +0530

    Unit test complete - JavaScript

--- Session-01/language/src/calculator.js
+++ Session-01/language/src/calculator.js
@@ -1,6 +1,8 @@
-//Should be able to treat non numeric string as zero
 function add(x, y) {
-    x = isNaN(x) ? 0 : parseInt(x, 10);
-    y = isNaN(y) ? 0 : parseInt(y, 10);
-    return x + y;
+    function parseArg(n) {
+        if (Array.isArray(n)) return add.apply(this, n); // Array is converted into argument list.
+        if (typeof n === 'function') return parseArg(n());
+        return isNaN(n) ? 0 : parseInt(n);
+    }
+    return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments, 1));
 }
